1c87dbad2ef9e4851f3807ecb5bc5602f503a7d0
Daan van Berkel <info@daanvanberkel.nl>
2020-02-04 13:30:41 +0100

    Cors regels toegevoegd

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const passportMiddleware = require('./src/passport');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/html/spa.html');
